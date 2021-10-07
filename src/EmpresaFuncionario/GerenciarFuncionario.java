fbdfb68095360c2dc58832f2c9967c746d82e497
Victor Balbino <victorbalbinoaraujo1@outlook.com>
2021-10-06 22:13:44 -0300

    Tentando resolver o input nextLine

--- src/EmpresaFuncionario/GerenciarFuncionario.java
+++ src/EmpresaFuncionario/GerenciarFuncionario.java
@@ -7,113 +7,122 @@ import java.util.Scanner;
 public class GerenciarFuncionario {
 
     public static void main(String[] args) {
-        Funcionario func = new Funcionario();
-        Empresa empresa = new Empresa();
+        boolean status = true;
 
-        System.out.println("Digite a opção desejada: ");
-        System.out.println("1 - Cadastrar funcionário");
-        System.out.println("2 - Cadastrar Empresa");
-        System.out.println("3 - Atualizar Salário");
-        System.out.println("4 - Demitir Funcionário");
-        System.out.println("5 - Listar Funcionário");
-        System.out.println("6 - Listar Funcionário de todas as empresas");
-        System.out.println("7 - Adicionar funcionário ao quadro da empresa");
-        System.out.println("8 - Verificar existência de funcionário na empresa");
-        System.out.println("9 - FINALIZAR");
+        while(status) {
+            Funcionario func = new Funcionario();
+            Empresa empresa = new Empresa();
 
-        System.out.println("Entre com sua opção: ");
-        Scanner entrada = new Scanner(System.in);
-        int option  = entrada.nextInt();
+            System.out.println();
+            System.out.println("Digite a opção desejada: ");
+            System.out.println("1 - Cadastrar funcionário");
+            System.out.println("2 - Cadastrar Empresa");
+            System.out.println("3 - Atualizar Salário");
+            System.out.println("4 - Demitir Funcionário");
+            System.out.println("5 - Listar Funcionário");
+            System.out.println("6 - Listar Funcionário de todas as empresas");
+            System.out.println("7 - Adicionar funcionário ao quadro da empresa");
+            System.out.println("8 - Verificar existência de funcionário na empresa");
+            System.out.println("9 - FINALIZAR");
 
-        switch (option) {
-            case 1:
-            // Cadastrar funcionário
-            System.out.println("Digite o ID do funcionário: ");
-            int idfuncionario = entrada.nextInt();
-            func.setIdFunc(idfuncionario);
+            System.out.println("Entre com sua opção: ");
+            Scanner entrada = new Scanner(System.in);
+            int option = entrada.nextInt();
 
-            System.out.println("Digite o salário do funcionário: ");
-            double salario = entrada.nextInt();
-            func.setSalario(salario);
-            
-            func.setEstaAtivo(true);
-            
-            System.out.println("Digite o nome do funcionário: ");
-            String nome = entrada.next();
-            func.setNomeFunc(nome);
+            switch (option) {
+                case 1:
+                    // Cadastrar funcionário
+                    System.out.println("Digite o ID do funcionário: ");
+                    int idfuncionario = entrada.nextInt();
+                    func.setIdFunc(idfuncionario);
 
-            System.out.println("Digite o departamento do funcionário: ");
-            String departamentoF = entrada.next();
-            func.setDepartamento(departamentoF);
+                    System.out.println("Digite o salário do funcionário: ");
+                    double salario = entrada.nextDouble();
+                    func.setSalario(salario);
 
-            System.out.println("Digite o número do documento do funcionário: ");
-            String doc = entrada.next();
-            func.setDocumento(doc);
+                    System.out.println("Digite o nome do funcionário: ");
 
-            func.setDataContratacao(LocalDate.now());
-            break;
+                    if(entrada.hasNextLine()) {
+                        String nome = entrada.nextLine();
+                        func.setNomeFunc(nome);
+                    }
 
-            case 2:
-            // Cadastrar Empresa
-            System.out.println("Digite o id da Empresa: ");
-            String IdEmpresa = entrada.next();
-            empresa.setIdEmpresa(IdEmpresa);
+                    System.out.println("Digite o departamento do funcionário: ");
+                    String departamentoF = entrada.nextLine();
+                    func.setDepartamento(departamentoF);
 
-            System.out.println("Digite a Razão Social da Empresa: ");
-            String Rsocial = entrada.next();
-            empresa.setRazaoSocial(Rsocial);
+                    System.out.println("Digite o número do documento do funcionário: ");
+                    String doc = entrada.nextLine();
+                    func.setDocumento(doc);
 
-            System.out.println("Digite o CNPJ da empresa: ");
-            String cnpj = entrada.next();
-            empresa.setCnpj(cnpj);
-            break;
+                    func.setDataContratacao(LocalDate.now());
+                    func.setEstaAtivo(true);
 
-            case 3:
-            System.out.println("Digite o novo salário do funcionário");
-            double novoSalario = entrada.nextInt();
-            func.atualizarSalario(novoSalario);
-            break;
+                    break;
 
-            case 4:
-            System.out.println("Demitir Funcionário");
-            System.out.println("Deseja mesmo demitir esse funcionário? (Sim ou Não)");
-            String decisao = entrada.next();
-            if (Objects.equals(decisao, "Sim")){
-                func.demitirFuncionario();
+                case 2:
+                    // Cadastrar Empresa
+                    System.out.println("Digite o id da Empresa: ");
+                    String IdEmpresa = entrada.next();
+                    empresa.setIdEmpresa(IdEmpresa);
+
+                    System.out.println("Digite a Razão Social da Empresa: ");
+                    String Rsocial = entrada.next();
+                    empresa.setRazaoSocial(Rsocial);
+
+                    System.out.println("Digite o CNPJ da empresa: ");
+                    String cnpj = entrada.next();
+                    empresa.setCnpj(cnpj);
+                    break;
+
+                case 3:
+                    System.out.println("Digite o novo salário do funcionário");
+                    double novoSalario = entrada.nextInt();
+                    func.atualizarSalario(novoSalario);
+                    break;
+
+                case 4:
+                    System.out.println("Demitir Funcionário");
+                    System.out.println("Deseja mesmo demitir esse funcionário? (Sim ou Não)");
+                    String decisao = entrada.next();
+                    if (Objects.equals(decisao, "Sim")) {
+                        func.demitirFuncionario();
+                    }
+                    break;
+
+                case 5:
+                    // listar funcionário
+                    func.imprimirFuncionario();
+                    break;
+
+                case 6:
+                    System.out.println("Listar Funcionário de todas as empresas");
+                    empresa.mostrarTodosEmpregados();
+                    break;
+
+                case 7:
+                    System.out.println("Adicionar funcionário ao quadro da empresa");
+                    empresa.adicionarEmpregados(func, empresa);
+                    break;
+
+                case 8:
+                    System.out.println("Verificar existência de funcionário na empresa");
+                    if (empresa.contemFuncionario(func)) {
+                        System.out.println("Funcionário existente");
+
+                    } else {
+                        System.out.println("Funcionário não encontrado!");
+                    }
+                    break;
+
+                case 9:
+                    System.out.println("FINALIZAR");
+                    status = false;
+                    break;
+
+                default:
+                    System.out.println("Desculpe, não consegui entender o que quis dizer. Por favor tente novamente.");
             }
-            break;
-
-            case 5:
-            // listar funcionário
-            func.imprimirFuncionario();
-            break;
-
-            case 6:
-            System.out.println("Listar Funcionário de todas as empresas");
-            empresa.mostrarTodosEmpregados();
-            break;
-
-            case 7:
-            System.out.println("Adicionar funcionário ao quadro da empresa");
-            empresa.adicionarEmpregados (func, empresa);
-            break;
-
-            case 8:
-            System.out.println("Verificar existência de funcionário na empresa");
-            if (empresa.contemFuncionario (func)) {
-                System.out.println("Funcionário existente");
-
-            } else {
-                System.out.println("Funcionário não encontrado!");
-            }    
-            break;
-
-            case 9:
-            System.out.println("FINALIZAR");
-            break;
-
-            default:
-            System.out.println("Desculpe, não consegui entender o que quis dizer. Por favor tente novamente.");
         }
     }
 }
